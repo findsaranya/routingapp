ad5caaa666443522eee90db1b08bf99aed6237b9
saranya <saranya@tangledai.com>
2021-06-27 13:21:22 +0530

    unscubscribe rsjx

--- src/app/users/user/user.component.ts
+++ src/app/users/user/user.component.ts
@@ -1,14 +1,15 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute,Params } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
-export class UserComponent implements OnInit {
+export class UserComponent implements OnInit,OnDestroy {
   user: {id: number, name: string};
-
+  paramSubscribe:Subscription;
   constructor(private route:ActivatedRoute) { }
 
   ngOnInit() {
@@ -17,6 +18,15 @@ export class UserComponent implements OnInit {
       id : this.route.snapshot.params['id'],
       name:this.route.snapshot.params['name']
     }
+    this.paramSubscribe = this.route.params
+    .subscribe((params:Params) => {
+        this.user['id'] = params['id'];
+        this.user['name']= params['name'];
+    })
+  }
+  ngOnDestroy(){
+    this.paramSubscribe.unsubscribe();
+
   }
 
 }
