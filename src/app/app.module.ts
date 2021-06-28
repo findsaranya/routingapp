2e45711d3ab3d366296203b34056c1448eda0fe9
saranya <saranya@tangledai.com>
2021-06-28 10:18:52 +0530

    routing module component changed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,52 +11,9 @@ import { EditServerComponent } from './servers/edit-server/edit-server.component
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
 import { FormsModule } from '@angular/forms';
-import {  RouterModule, Routes } from '@angular/router';
+//import {  RouterModule, Routes } from '@angular/router';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-const appRoutes:Routes = [
-  {
-    path:"",
-    component:HomeComponent
-  },
-  {
-    path:"users",
-    component:UsersComponent,
-    children:[
-        {
-    path:":id/:name",
-    component:UserComponent
-  },
-    ]
-  },
- 
-  {
-    path:"servers",
-    component:ServersComponent,
-    children:[
-        {
-    path:":id",
-    component:ServerComponent
-  },
-  {
-    path:":id/edit",
-    component:EditServerComponent
-  }
-    ]
-  },
-  {
-    path : "not-found",
-    component:PageNotFoundComponent
-  },
-  {
-    path : "",
-    redirectTo:'not-found',
-    pathMatch:'full'
-  },
-  {
-    path:'**',
-    redirectTo:'not-found'
-  }
-];
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -72,7 +29,7 @@ const appRoutes:Routes = [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    RouterModule.forRoot(appRoutes),
+    //RouterModule.forRoot(appRoutes),
   ],
   providers: [ServersService],
   bootstrap: [AppComponent]
